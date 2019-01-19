15aeb1a8aa55c991ff9a845eae80f2143173d4ff
ascuccimarra <alscuy@gmail.com>
2019-01-19 17:40:19 +0100

    Clients, Directives, Reviews, Technologies, Stats API Tests

---
+++ tests/api/clientsApi.test.js
@@ -0,0 +1,193 @@
+const request = require('supertest')
+const db = require('../../models')
+const app = require('../../index')
+const Util = require('../testUtil.js')
+
+const apiUrl = '/api/clients'
+const clientSchema = {
+  id: '',
+  name: '',
+  ownerId: '',
+  teamLeadId: '',
+  pictureUrl: '',
+  slackInternalChannel: '',
+  slackClientChannel: '',
+  principals: '',
+  technologies: '',
+  organizationId: '',
+}
+
+const principalSchema = {
+  id: '',
+  firstName: '',
+  lastName: '',
+}
+
+const technologySchema = {
+  id: '',
+  name: '',
+  description: '',
+}
+
+beforeAll(async () => {
+  // Code that runs before every test goes here
+})
+
+afterAll(() => {
+
+})
+
+describe('List Clients', () => {
+  test('respond with json containing a list of all clients', async (done) => {
+    const dbCli = await Util.createTestClient()
+    expect(dbCli.id).toBeDefined()
+
+    request(app)
+      .get(apiUrl)
+      .expect('Content-Type', /json/)
+      .expect(200, (err, res) => {
+        expect(err).toBeNull()
+        Util.validateSchema(res.body, { count: '', rows: '' })
+        const list = res.body.rows
+        expect(res.body.count).not.toBeLessThan(1)
+        expect(list.length).not.toBeLessThan(1)
+        Util.validateSchema(list[0], clientSchema)
+        const result = list.find(cli => cli.id === dbCli.id)
+        expect(result).not.toBeUndefined()
+        done(err)
+      })
+  })
+})
+
+describe('View Client', () => {
+  test('respond with json with the client', async (done) => {
+    const dbCli = await Util.createTestClient()
+    const principals = await dbCli.getPrincipals()
+    const techs = await dbCli.getTechnologies()
+
+
+    request(app)
+      .get(`${apiUrl}/${dbCli.id}`)
+      .expect('Content-Type', /json/)
+      .expect(200, (err, res) => {
+        expect(err).toBeNull()
+        Util.validateSchema(res.body, clientSchema)
+        expect(res.body.id).toEqual(dbCli.id)
+        expect(res.body.name).toEqual(dbCli.name)
+
+        expect(res.body.principals.length).toBe(2)
+        Util.validateSchema(res.body.principals[0], principalSchema)
+        expect(res.body.principals[0].id).toEqual(principals[0].id)
+        expect(res.body.principals[1].id).toEqual(principals[1].id)
+
+        expect(res.body.technologies.length).toBe(2)
+        Util.validateSchema(res.body.technologies[0], technologySchema)
+        expect(res.body.technologies[0].id).toEqual(techs[0].id)
+        expect(res.body.technologies[1].id).toEqual(techs[1].id)
+        done(err)
+      })
+  })
+})
+
+describe('Create Client', () => {
+  test('create client returns 201 and the client json', async (done) => {
+    const u1 = await Util.createTestUser()
+    const t1 = await Util.createTestTechnology()
+
+    const client = {
+      name: 'create cli name',
+      ownerId: u1.id,
+      teamLeadId: u1.id,
+      principals: [
+        { id: u1.id },
+      ],
+      technologies: [
+        { id: t1.id },
+      ],
+    }
+    request(app)
+      .post(apiUrl)
+      .send(client)
+      .expect('Content-Type', /json/)
+      .expect(201, (err, res) => {
+        expect(err).toBeNull()
+        Util.validateSchema(res.body, clientSchema)
+        expect(res.body.id).not.toBeLessThan(1)
+        expect(res.body.name).toEqual(client.name)
+        expect(res.body.principals.length).toBe(1)
+        expect(res.body.principals[0].id).toEqual(u1.id)
+        expect(res.body.technologies.length).toBe(1)
+        expect(res.body.technologies[0].id).toEqual(t1.id)
+        done(err)
+      })
+  })
+
+  test('create client with existing name fails and returns 422', async (done) => {
+    const dbCli = await Util.createTestClient()
+    const u1 = await Util.createTestUser()
+
+    const client = {
+      name: dbCli.name,
+      ownerId: u1.id,
+      teamLeadId: u1.id,
+    }
+    request(app)
+      .post(apiUrl)
+      .send(client)
+      .expect('Content-Type', /json/)
+      .expect(422, (err, res) => {
+        console.log(err)
+        done(err)
+      })
+  })
+})
+
+describe('Update Client', () => {
+  test('update client returns 200 and the client json', async (done) => {
+    const dbCli = await Util.createTestClient()
+    const u1 = await Util.createTestUser()
+    const t1 = await Util.createTestTechnology()
+
+    const json = {
+      name: 'new updated cli name',
+      principals: [
+        { id: u1.id },
+      ],
+      technologies: [
+        { id: t1.id },
+      ],
+    }
+
+    request(app)
+      .put(`${apiUrl}/${dbCli.id}`)
+      .send(json)
+      .expect('Content-Type', /json/)
+      .expect(200, (err, res) => {
+        expect(err).toBeNull()
+        Util.validateSchema(res.body, clientSchema)
+        expect(res.body.id).toEqual(dbCli.id)
+        expect(res.body.name).toEqual(json.name)
+        expect(res.body.principals.length).toBe(1)
+        expect(res.body.principals[0].id).toEqual(u1.id)
+        expect(res.body.technologies.length).toBe(1)
+        expect(res.body.technologies[0].id).toEqual(t1.id)
+        done(err)
+      })
+  })
+})
+
+describe('Delete Client', () => {
+  test('delete client returns 200 and the client is not returned ', async (done) => {
+    const dbCli = await Util.createTestClient()
+
+    const res = await request(app)
+      .delete(`${apiUrl}/${dbCli.id}`)
+    expect(res.statusCode).toBe(200)
+
+    const res2 = await request(app)
+      .get(`${apiUrl}/${dbCli.id}`)
+    expect(res2.statusCode).toBe(404)
+
+    done()
+  })
+})
---
+++ tests/api/reviewsApi.test.js
@@ -0,0 +1,173 @@
+const request = require('supertest')
+const db = require('../../models')
+const app = require('../../index')
+const Util = require('../testUtil.js')
+
+const apiUrlClients = '/api/clients'
+const reviewSchema = {
+  id: '',
+  reviewDate: '',
+  reviewerId: '',
+  clientId: '',
+  directives: '',
+  link: '',
+  score: '',
+  trend: '',
+}
+
+const directiveSchema = {
+  id: '',
+  title: '',
+  subtitle: '',
+  ReviewDirective: '',
+}
+
+beforeAll(async () => {
+  // Code that runs before every test goes here
+})
+
+afterAll(() => {
+
+})
+
+describe('List Reviews for a client', () => {
+  test('respond with json containing a list of all reviews', async (done) => {
+    const dbReview = await Util.createTestReview()
+    expect(dbReview.id).toBeDefined()
+
+    request(app)
+      .get(`${apiUrlClients}/${dbReview.clientId}/reviews`)
+      .expect('Content-Type', /json/)
+      .expect(200, (err, res) => {
+        expect(err).toBeNull()
+        Util.validateSchema(res.body, { count: '', rows: '' })
+        const list = res.body.rows
+        expect(res.body.count).not.toBeLessThan(1)
+        expect(list.length).not.toBeLessThan(1)
+        Util.validateSchema(list[0], reviewSchema)
+        const result = list.find(cli => cli.id === dbReview.id)
+        expect(result).not.toBeUndefined()
+        done(err)
+      })
+  })
+})
+
+describe('View Review', () => {
+  test('respond with json with the review', async (done) => {
+    const dbReview = await Util.createTestReview()
+    const directives = await dbReview.getDirectives()
+
+
+    request(app)
+      .get(`${apiUrlClients}/${dbReview.clientId}/reviews/${dbReview.id}`)
+      .expect('Content-Type', /json/)
+      .expect(200, (err, res) => {
+        expect(err).toBeNull()
+        Util.validateSchema(res.body, reviewSchema)
+        expect(res.body.id).toEqual(dbReview.id)
+        expect(res.body.clientId).toEqual(dbReview.clientId)
+
+        expect(res.body.directives.length).toBe(3)
+        Util.validateSchema(res.body.directives[0], directiveSchema)
+        expect(res.body.directives[0].id).toEqual(directives[0].id)
+        expect(res.body.directives[1].id).toEqual(directives[1].id)
+
+        Util.validateSchema(res.body.directives[0], directiveSchema)
+        expect(res.body.directives[0].ReviewDirective.compliant)
+          .toEqual(directives[0].ReviewDirective.compliant)
+
+        done(err)
+      })
+  })
+})
+
+describe('Create Review', () => {
+  test('create review returns 201 and the review json', async (done) => {
+    const u1 = await Util.createTestUser()
+    const c1 = await Util.createTestClient()
+    const d1 = await Util.createTestDirective()
+
+    const review = {
+      reviewerId: u1.id,
+      clientId: c1.id,
+      reviewDate: new Date(),
+      directives: [
+        {
+          id: d1.id,
+          compliant: true,
+          notes: 'Just a note',
+        },
+      ],
+    }
+    request(app)
+      .post(`${apiUrlClients}/${c1.id}/reviews`)
+      .send(review)
+      .expect('Content-Type', /json/)
+      .expect(201, (err, res) => {
+        expect(err).toBeNull()
+        Util.validateSchema(res.body, reviewSchema)
+        expect(res.body.id).not.toBeLessThan(1)
+        expect(res.body.clientId).toEqual(c1.id)
+        expect(parseInt(res.body.score, 10)).toBe(5)
+        expect(res.body.directives.length).toBe(1)
+        expect(res.body.directives[0].id).toEqual(d1.id)
+        Util.validateSchema(res.body.directives[0], directiveSchema)
+        expect(res.body.directives[0].ReviewDirective.compliant).toBeTruthy()
+        expect(res.body.directives[0].ReviewDirective.notes).toEqual('Just a note')
+
+        done(err)
+      })
+  })
+})
+
+describe('Update Review', () => {
+  test('update review returns 200 and the review json', async (done) => {
+    const dbReview = await Util.createTestReview()
+    const d1 = await Util.createTestDirective()
+    const u1 = await Util.createTestUser()
+
+    const json = {
+      reviewerId: u1.id,
+      directives: [
+        {
+          id: d1.id,
+          compliant: true,
+          notes: 'Updated note',
+        },
+      ],
+    }
+
+    request(app)
+      .put(`${apiUrlClients}/${dbReview.clientId}/reviews/${dbReview.id}`)
+      .send(json)
+      .expect('Content-Type', /json/)
+      .expect(200, (err, res) => {
+        expect(err).toBeNull()
+        Util.validateSchema(res.body, reviewSchema)
+        expect(res.body.id).toEqual(dbReview.id)
+        expect(res.body.reviewerId).toEqual(u1.id)
+        expect(res.body.directives.length).toBe(1)
+        expect(res.body.directives[0].id).toEqual(d1.id)
+        expect(res.body.directives[0].ReviewDirective.compliant).toBeTruthy()
+        expect(res.body.directives[0].ReviewDirective.notes).toEqual('Updated note')
+
+        done(err)
+      })
+  })
+})
+
+describe('Delete Review', () => {
+  test('delete review returns 200 and the review is not returned ', async (done) => {
+    const dbReview = await Util.createTestReview()
+
+    const res = await request(app)
+      .delete(`${apiUrlClients}/${dbReview.clientId}/reviews/${dbReview.id}`)
+    expect(res.statusCode).toBe(200)
+
+    const res2 = await request(app)
+      .delete(`${apiUrlClients}/${dbReview.clientId}/reviews/${dbReview.id}`)
+    expect(res2.statusCode).toBe(404)
+
+    done()
+  })
+})
